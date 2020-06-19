4135b3b2013a463c7ac89484df4804c8d9774a73
shlkeyboardhero <64306629+shlkeyboardhero@users.noreply.github.com>
2020-06-19 20:13:41 +0300

    Update Item.java

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -3,9 +3,10 @@ package com.company;
 public class Item {
     String key;
     int value;
+    boolean deleted = false;
 
-    public Item(String key1, int value1){
-        this.key=key1;
-        this.value=value1;
+    public Item(String key, int value1) {
+        this.key = key;
+        this.value = value1;
     }
 }
