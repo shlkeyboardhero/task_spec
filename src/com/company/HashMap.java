d3fb356744232ac6c8b5c2d9667c21b2ee31fe63
shlkeyboardhero <64306629+shlkeyboardhero@users.noreply.github.com>
2020-06-20 13:15:50 +0300

    Add files via upload

---
+++ src/com/company/HashMap.java
@@ -0,0 +1,164 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HashMap {
+    //static final int Step = 1;
+    int probingStep;
+    int bucketSize = 23;
+
+    private Item[] buckets;
+
+    /*public HashMapSecondOwnImp() {
+        this.step = Step;
+        buckets = new Item[bucketSize];
+    }*/
+
+    public HashMap(int probingStep) {
+        this.probingStep = probingStep;
+        bucketSize = resizeBuckets(bucketSize, probingStep);
+        buckets = new Item[bucketSize];
+    }
+
+
+    public boolean coprimeNumberCheck(int a, int b) {
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        if (a == 1)
+            return true;
+        else
+            return false;
+    }
+
+    public int resizeBuckets(int bucketSize, int probingStep) {
+        if (probingStep >= bucketSize) {
+            bucketSize = probingStep + 1;
+        }
+        while (true) {
+            if (!coprimeNumberCheck(bucketSize, probingStep)) {
+                bucketSize += probingStep;
+            } else
+                return bucketSize;
+        }
+    }
+
+
+    private int computeIndexViaItem(Item selectedItem, int tryPlaceItemToTable) {
+
+        return computeIndexViaKey(selectedItem.key, tryPlaceItemToTable);
+    }
+
+    private int computeIndexViaKey(String key, int tryPlaceItemToTable) {
+
+        return (key.hashCode() + tryPlaceItemToTable * probingStep) % bucketSize;
+    }
+
+    public void putNewItem(String key, int value) {
+        Item newItem = new Item(key, value);
+        if (insertItem(newItem) >= bucketSize) {
+            bucketSize += probingStep;
+            buckets = moveToNewBuckets();
+            for (int j = 0; j < bucketSize; j++) {
+                int newItemIndex = computeIndexViaItem(newItem, j);
+                if (buckets[newItemIndex] == null || buckets[newItemIndex].deleted) {
+                    buckets[newItemIndex] = newItem;
+                    break;
+                }
+            }
+        }
+
+    }
+
+
+    private Item[] moveToNewBuckets() {
+        int currentIndex;
+        Item newBuckets[] = new Item[bucketSize];
+        for (int k = 0; k < buckets.length; k++) {
+            if (buckets[k] != null) {
+                for (int j = 0; j < bucketSize; j++) {
+                    currentIndex = computeIndexViaItem(buckets[k], j);
+                    if (newBuckets[currentIndex] == null) {
+                        newBuckets[currentIndex] = buckets[k];
+                        break;
+                    }
+                }
+            }
+
+        }
+        return newBuckets;
+    }
+
+    private int insertItem(Item newItem) {
+        int j;
+        int currentIndex;
+        for (j = 0; j < bucketSize; j++) {
+            currentIndex = computeIndexViaItem(newItem, j);
+            if (buckets[currentIndex] == null || buckets[currentIndex].deleted) {
+                buckets[currentIndex] = newItem;
+                break;
+            } else {
+                if (buckets[currentIndex].key.equals(newItem.key)) {
+                    buckets[currentIndex] = newItem;
+                    break;
+                }
+            }
+        }
+        return j;
+    }
+
+    public void delItem(String key) {
+        int nextIndex;
+        for (int tryPlaceItemToTable = 0; tryPlaceItemToTable < bucketSize; tryPlaceItemToTable++) {
+            nextIndex = computeIndexViaKey(key, tryPlaceItemToTable);
+            if (key.equals(buckets[nextIndex].key)) {
+                buckets[nextIndex].deleted = true;
+                break;
+            }
+
+        }
+    }
+
+
+    public Integer getValue(String key) {
+        int currentIndex;
+        for (int tryPlaceItemToTable = 0; tryPlaceItemToTable < bucketSize; tryPlaceItemToTable++) {
+            currentIndex = computeIndexViaKey(key, tryPlaceItemToTable);
+            if (!buckets[currentIndex].deleted && key.equals(buckets[currentIndex].key)) {
+                return buckets[currentIndex].value;
+            }
+        }
+        return null;
+    }
+
+    public int getSize() {
+        return getKeys().size();
+    }
+
+    public List<String> getKeys() {
+
+        ArrayList<String> keysValue = new ArrayList<String>();
+        for (int j = 0; j < bucketSize; j++) {
+            if (buckets[j] != null && !buckets[j].deleted) {
+                keysValue.add(buckets[j].key);
+            }
+        }
+        return keysValue;
+    }
+
+    public List<Integer> getValues() {
+
+        ArrayList<Integer> valuesValue = new ArrayList<Integer>();
+        for (int j = 0; j < bucketSize; j++) {
+            if (buckets[j] != null && !buckets[j].deleted) {
+                valuesValue.add(buckets[j].value);
+            }
+        }
+        return valuesValue;
+    }
+}
+
